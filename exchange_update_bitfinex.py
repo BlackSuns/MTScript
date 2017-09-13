220ebdcf088ed43b9c1fcb196974fdf5edf38962
Larry Guo <larryrun80@gmail.com>
2017-09-13 13:32:22 +0800

    refine output

--- exchange_update_bitfinex.py
+++ exchange_update_bitfinex.py
@@ -1,6 +1,8 @@
+from utils import print_log
 from exchanges.bitfinex import BitfinexExchange
 
 
 if __name__ == '__main__':
+    print_log("start sync exchanges...")
     bf = BitfinexExchange()
     bf.post_result()
