cac7aaf3691ce78b8244eeb24a72c28b0feec62e
Larry Guo <larryrun80@gmail.com>
2017-10-25 23:40:06 +0800

    update config

--- exchange_update_bitfinex.py
+++ exchange_update_bitfinex.py
@@ -1,8 +1,8 @@
-import random
+# import random
 
 from utils import print_log
 from exchanges.bitfinex import BitfinexExchange
-from exchanges.bitfinex_probe import BitfinexProbeExchange
+# from exchanges.bitfinex_probe import BitfinexProbeExchange
 
 
 if __name__ == '__main__':
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     bf = BitfinexExchange()
     bf.post_result_batch()
 
-    if random.randint(0, 1000) < 5:
-        bfp = BitfinexProbeExchange()
-        bfp.post_result_batch()
-        print_log("probed...")
+    # if random.randint(0, 1000) < 5:
+    #     bfp = BitfinexProbeExchange()
+    #     bfp.post_result_batch()
+    #     print_log("probed...")
 
     print_log("Bitfinex Done...")
