4adcb0aec6a780ec487491e3a4331fe46328a235
Larry Guo <larryrun80@gmail.com>
2017-11-13 17:57:42 +0800

    update coinegg

--- exchanges/coinegg.py
+++ exchanges/coinegg.py
@@ -12,46 +12,30 @@ class CoineggExchange(BaseExchange):
         self.exchange_id = 1346
         self.base_url = 'https://api.coinegg.com/api/v1'
 
-        self.ticker_url = '/ticker'
+        self.ticker_url = '/allticker'
 
         self.alias = 'COINEGG'
         self.with_name = False
         self.exchange_conf = os.path.abspath(os.path.dirname(__file__)) +\
             '/exchange_conf/{}.json'.format(self.exchange)
 
-    def get_available_symbol(self):
-        with open(self.exchange_conf, 'r') as f:
-            data = json.load(f)
-
-        return data
-
     def get_remote_data(self):
+        url = '{}{}'.format(
+            self.base_url, self.ticker_url)
+        return self.ticker_callback(self.get_json_request(url))
+
+    def ticker_callback(self, result):
         return_data = []
         anchor = 'BTC'
-        conf_data = self.get_available_symbol()
-        total = len(conf_data['name_symbol'])
-        dealed = 1
-        for k in conf_data['name_symbol'].keys():
-            url = '{}{}?coin={}'.format(self.base_url,
-                                        self.ticker_url,
-                                        str(k).lower())
 
-            print('dealing {}/{}: {}'.format(dealed, total, url))
-            dealed += 1
-            try:
-                r = self.get_json_request(url)
-                # print(r)
-                price = float(r['last'])
-                volume = float(r['vol'])
+        for s in result.keys():
+            pair = '{}/{}'.format(s.upper(), anchor)
+            return_data.append({
+                'pair': pair,
+                'price': float(result[s]['last']),
+                'volume_anchor': float(result[s]['volume']),
+                'volume': float(result[s]['vol']),
+            })
 
-                data = {
-                    'pair': '{}/{}'.format(k.upper(), anchor.upper()),
-                    'price': price,
-                    'volume': volume,
-                    'volume_anchor': price * volume
-                }
-
-                return_data.append(data)
-            except Exception as e:
-                print('error happened, {}'.format(e))
+        # print(return_data)
         return return_data
