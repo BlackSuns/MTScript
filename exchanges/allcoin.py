5193e874214c9490c4122b868a78453a69ea1beb
Larry Guo <larryrun80@gmail.com>
2017-11-27 19:41:32 +0800

    refine allcoin

--- exchanges/allcoin.py
+++ exchanges/allcoin.py
@@ -9,40 +9,28 @@ class AllcoinExchange(BaseExchange):
         super().__init__()
         self.exchange = 'allcoin'
         self.exchange_id = 41
-        self.base_url = 'https://api.allcoin.com/api/v1'
+        self.base_url = 'https://www.allcoin.com/Home'
 
-        self.ticker_url = '/ticker'
+        self.ticker_url = '/MarketOverViewDetail/'
 
         self.alias = 'Allcoin'
         self.with_name = False
         self.exchange_conf = os.path.abspath(os.path.dirname(__file__)) +\
             '/exchange_conf/{}.json'.format(self.exchange)
 
-    def get_available_pair(self):
-        with open(self.exchange_conf, 'r') as f:
-            data = json.load(f)
-
-        return data
-
     def get_remote_data(self):
         return_data = []
-        pairs = self.get_available_pair()['pairs']
-        for i, p in enumerate(pairs, 1):
-            print('dealing {}/{} pair: {}'.format(i, len(pairs), p))
-            try:
-                (symbol, anchor) = p.split('_')
-                url = '{}{}?symbol={}'.format(self.base_url, self.ticker_url, p)
-                # print(url)
-                r = self.get_json_request(url)
+        url = '{}{}'.format(self.base_url, self.ticker_url)
+        r = self.get_json_request(url)
+        # print(r)
 
-                data = {
-                    'pair': '{}/{}'.format(symbol.upper(), anchor.upper()),
-                    'price': r['ticker']['last'],
-                    'volume': r['ticker']['vol'],
-                    'volume_anchor': r['ticker']['last'] * r['ticker']['vol']
-                }
+        for market in r['marketCoins']:
+            for m in market['Markets']:
+                return_data.append({
+                    'pair': '{}/{}'.format(m['Market']['Primary'].upper(), m['Market']['Secondary'].upper()),
+                    'price': float(m['LastPrice']),
+                    'volume': float(m['Volumn24H']),
+                    'volume_anchor': float(m['Amount24H'])
+                })
 
-                return_data.append(data)
-            except Exception as e:
-                print('error happened, exist {}: {}'.format(p, e))
         return return_data
