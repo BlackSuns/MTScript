5107e8732ac98275409e509243327c0f141c9d8f
Larry Guo <larryrun80@gmail.com>
2017-12-05 00:55:21 +0800

    update conf

--- exchanges/zaif.py
+++ exchanges/zaif.py
@@ -12,6 +12,7 @@ class ZaifExchange(BaseExchange):
         self.base_url = 'https://api.zaif.jp/api/1'
 
         self.ticker_url = '/ticker'
+        self.pair_url = '/currency_pairs/all'
 
         self.alias = ''
         self.with_name = False
@@ -19,32 +20,41 @@ class ZaifExchange(BaseExchange):
             '/exchange_conf/{}.json'.format(self.exchange)
 
     def get_available_pairs(self):
-        with open(self.exchange_conf, 'r') as f:
-            data = json.load(f)
+        url = '{}{}'.format(self.base_url, self.pair_url)
+        return self.pair_callback(self.get_json_request(url))
 
-        return data['pairs']
+    def pair_callback(self, result):
+        support_pairs = {}
+        for r in result:
+            (symbol, anchor) = r['name'].split('/')
+            if symbol == 'BITCRYSTALS':
+                symbol = 'BCY'
+
+            pair = '{}/{}'.format(symbol.upper(), anchor.upper())
+            print(symbol, anchor, pair)
+            support_pairs[pair] = r['currency_pair']
+
+        return support_pairs
 
     def get_remote_data(self):
         return_data = []
         pairs = self.get_available_pairs()
-        for i, p in enumerate(pairs, 1):
+        print(pairs)
+        for i, p in enumerate(pairs.keys(), 1):
             # print('dealing {}/{} pair: {}'.format(i, len(pairs), p))
             try:
-                (symbol, anchor) = p.split('_')
                 url = '{}{}/{}'.format(self.base_url,
                                        self.ticker_url,
-                                       str(p).lower())
+                                       pairs[p])
                 # print(url)
                 r = self.get_json_request(url)
-
-                price = r['last']
-                volume = r['volume']
+                # print(r)
 
                 data = {
-                    'pair': '{}/{}'.format(symbol.upper(), anchor.upper()),
-                    'price': price,
-                    'volume': volume,
-                    'volume_anchor': price * volume
+                    'pair': p,
+                    'price': r['last'],
+                    'volume': r['volume'],
+                    'volume_anchor': r['last'] * r['vwap']
                 }
 
                 return_data.append(data)
