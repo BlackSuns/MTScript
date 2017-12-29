d45d28bcae6f31ebc59c2f4be52345b48e6c00e8
Larry Guo <larryrun80@gmail.com>
2017-12-29 23:58:49 +0800

    adjust uncoinex

--- exchanges/uncoinex.py
+++ exchanges/uncoinex.py
@@ -8,9 +8,9 @@ class UncoinexExchange(BaseExchange):
         super().__init__()
         self.exchange = 'uncoinex'
         self.exchange_id = 1367
-        self.base_url = 'https://open.ucoins.cc/api/v1'
+        self.base_url = 'https://ucoins.cc/api/v1'
 
-        self.ticker_url = '/ticker'
+        self.ticker_url = '/coins'
 
         self.alias = '澳洲U网'
         self.with_name = False
@@ -25,16 +25,17 @@ class UncoinexExchange(BaseExchange):
     def ticker_callback(self, result):
         return_data = []
 
-        for r in result:
-            (symbol, anchor) = str(r['symbol']).split('_')
-            if anchor and symbol:
-                pair = '{}/{}'.format(symbol.upper(), anchor.upper())
-                return_data.append({
-                    'pair': pair,
-                    'price': float(r['price']),
-                    'volume_anchor': float(r['amount']),
-                    'volume': float(r['volume']),
-                })
+        for anchor in result.keys():
+            if anchor != 'btcPrice':
+                for t in result[anchor]:
+                    symbol = t['name']
+                    pair = '{}/{}'.format(symbol.upper(), anchor.upper())
+                    return_data.append({
+                        'pair': pair,
+                        'price': t['price'],
+                        'volume_anchor': t['amount'],
+                        'volume': t['volume'],
+                    })
 
         # print(return_data)
         return return_data
