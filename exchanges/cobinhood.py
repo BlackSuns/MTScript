f99a933e7143c7a07d0023a0177e2acb030dcf95
Larry Guo <larryrun80@gmail.com>
2018-01-09 16:49:54 +0800

    update config

--- exchanges/cobinhood.py
+++ exchanges/cobinhood.py
@@ -10,33 +10,40 @@ class CobinhoodExchange(BaseExchange):
         self.exchange_id = 1395
         self.base_url = 'https://api.cobinhood.com/v1'
 
-        self.ticker_url = '/market/stats'
+        self.pair_url = '/market/stats'
+        self.ticker_url = '/market/tickers'
 
         self.alias = ''
         self.with_name = False
         self.exchange_conf = os.path.abspath(os.path.dirname(__file__)) +\
             '/exchange_conf/{}.json'.format(self.exchange)
 
+    def get_available_pairs(self):
+        pairs = []
+        url = '{}{}'.format(self.base_url, self.pair_url)
+        # print(url)
+        r = self.get_json_request(url)
+        for p in r['result'].keys():
+            pairs.append(p)
+
+        return pairs
+
     def get_remote_data(self):
-        url = '{}{}'.format(
-            self.base_url, self.ticker_url)
-        print(url)
-        return self.ticker_callback(self.get_json_request(url))
-
-    def ticker_callback(self, result):
-        # print(result)
         return_data = []
+        pairs = self.get_available_pairs()
 
-        for k in result['result'].keys():
-            (symbol, anchor) = str(k).split('-')
-            if anchor and symbol:
-                pair = '{}/{}'.format(symbol.upper(), anchor.upper())
-                return_data.append({
-                    'pair': pair,
-                    'price': result['result'][k]['last_price'],
-                    'volume_anchor': float(result['result'][k]['quote_volume']),
-                    'volume': float(result['result'][k]['base_volume']),
-                })
+        for p in pairs:
+            url = '{}{}/{}'.format(self.base_url, self.ticker_url, p)
+            r = self.get_json_request(url)
+            (symbol, anchor) = p.split('-')
+            pair = '{}/{}'.format(symbol, anchor)
+            price = float(r['result']['ticker']['last_trade_price'])
+            volume = float(r['result']['ticker']['24h_volume'])
+            return_data.append({
+                "pair": pair,
+                "price": price,
+                "volume": volume,
+                "volume_anchor": price * volume,
+            })
 
-        # print(return_data)
         return return_data
